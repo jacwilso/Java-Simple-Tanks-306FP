29c48937a0d1f9ef1eff725da230663c03e8cc52
Delaney Marsh <dmarsh@mymail.mines.edu>
2016-11-30 15:28:15 -0700

    Started working on challenge

--- src/game/Challenge.java
+++ src/game/Challenge.java
@@ -1,16 +1,32 @@
 package game;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 
-public class Challenge extends JPanel{
+public class Challenge extends JDialog{
 	private JRadioButton angle1;
 	private JRadioButton angle2;
 	private JRadioButton angle3;
 	private Point targetPosition;
-	public static final double[] ANGLE_OPTIONS = {23,23,23,23,23};
+	public static ArrayList<Integer> angleOptions;
+	
+	public Challenge(){
+		for(int i = 0; i <= 90; i++){
+			angleOptions.add(i);
+		}
+		Collections.shuffle(angleOptions);
+	}
+	
+	public void updateAngleButtons(){
+		angle1= new JRadioButton(angleOptions.get(0).toString());
+	}
+	
+	
 	
 }
